af752b23dfdaf3041ce97b9fd8f34916b4a7f780
kozakd.02 <kozakd.02@B121-12>
2021-05-07 08:45:46 +0200

    PrvniTlac

--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Ahoj");
+            MessageBox.Show("AAhoj");
         }
     }
 }
